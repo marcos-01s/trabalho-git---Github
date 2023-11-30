0fc9be0ecd8517a1e475c58b7eb2df9968c7d84f
marcos-01s <152218041+marcos-01s@users.noreply.github.com>
2023-11-30 18:02:32 -0300

    Add files via upload

---
+++ package trabalho_java;.java
@@ -0,0 +1,18 @@
+package trabalho_java;
+
+public class Livro {
+    String Nome;
+    String Descrição;
+    String Autor;
+    String Isbn;
+    double Preço;
+    String DataPub;
+
+    void dadosLivro()  {
+        System.out.print1n("Nome do livro: " + Nome);
+        System.out.print1n("Preço: " + preço);
+        System.out.print1n("Autor " + outor);
+        System.out.print1n("Data de Publicação: " + DataPub + "\n");
+    }
+    
+}
